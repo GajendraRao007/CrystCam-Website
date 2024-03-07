8212ca6d0c7d35b18103cbd5b799677faba26866
Gajendra <raogajendra27@gmail.com>
2024-03-07 22:34:45 +0530

    working on products section

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ function mySidebar() {
         sidebar.style.right = '0px';
     }
 }
+
+
+let product1 = document.getElementById('pro1')
+
+product1.addEventListener('click', mybtn1)
+
+function mybtn1() {
+
+    let showDisplay = document.getElementById('display').value += 1;
+
+    document.getElementById('display').value = showDisplay;
+
+    console.log(showDisplay);
+
+    
+}
