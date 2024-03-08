0ecf7dc2f762caf257237df92aece90a1b1335b6
Gajendra <raogajendra27@gmail.com>
2024-03-08 12:51:59 +0530

    added the cartQuantity when the button is clicked

--- main.js
+++ main.js
@@ -16,13 +16,28 @@ let product1 = document.getElementById('pro1')
 
 product1.addEventListener('click', mybtn1)
 
+let displayElement = document.getElementById('display');
+let displayElement2 = document.getElementById('display2');
+
+let cartQuantity = document.getElementById('display')
+let cartQuantity2 = document.getElementById('display2')
+
 function mybtn1() {
+    let showDisplay = parseInt(displayElement.innerText || 0);
+    showDisplay += 1;
+    let showDisplay2 = parseInt(displayElement2.innerText || 0);
+    showDisplay2 += 1;
 
-    let showDisplay = document.getElementById('display').value += 1;
-
-    document.getElementById('display').value = showDisplay;
+    displayElement.innerText = showDisplay;
+    displayElement2.innerText = showDisplay2;
 
     console.log(showDisplay);
 
+    if (showDisplay === 1) {
+        cartQuantity.style.display = 'block';
+    }
+    if (showDisplay2 === 1) {
+        cartQuantity2.style.display = 'block';
+    }
     
 }
